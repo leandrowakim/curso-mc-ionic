c96a7c9e62579c11e212cd9f938147902e4f6610
leandrowakim <leandrowakim@gmail.com>
2021-07-13 23:15:37 -0300

    Refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -21,6 +21,10 @@ export class ProdutosPage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let categoria_id = this.navParams.get('categoria_id');
     let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
@@ -31,7 +35,7 @@ export class ProdutosPage {
       },
       error => {
         loader.dismiss();
-      });
+      });    
   }
 
   loadImageUrls() {
@@ -56,4 +60,11 @@ export class ProdutosPage {
     loader.present();
     return loader;
   }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+      refresher.complete();
+    }, 1000);
+  }  
 }
